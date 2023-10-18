5b023cdeb7e122df9048aee25ef5eddfdf64d528
Sergio Henrique <shs_28@hotmail.com>
2023-10-17 22:11:50 -0300

    Inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const categoryRoutes = require('./routes/categoryRoutes')
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello alteração!');
+})
+
+app.get('/categories', categoryRoutes);
+
+app.listen(port, () => {
+    console.log('Servidor rodando em http://localhost:' + port);
+})
